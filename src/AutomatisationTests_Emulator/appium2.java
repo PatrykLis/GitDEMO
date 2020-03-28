007766d25cd6fd1aa505e47c880fc59220ac3748
kesiloo <kesguardian@gmail.com>
2020-03-28 20:05:12 +0100

    repaired some files

--- src/AutomatisationTests_Emulator/appium2.java
+++ src/AutomatisationTests_Emulator/appium2.java
@@ -1,18 +1,17 @@
 package AutomatisationTests_Emulator;
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType.APP_PACKAGE;
-
 import io.appium.java_client.remote.MobilePlatform;
 
 public class appium2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 
 	
@@ -22,7 +21,7 @@ public class appium2 {
 	cap.setCapability(MobileCapabilityType.PLATFORM_NAME,MobilePlatform.ANDROID);
 	cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Andorid device");
 	cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "25");
-	cap.setCapability(MobileCapabilityType.APP, value);
+	cap.setCapability(MobileCapabilityType.APP, "value");
 	
 	AndroidDriver driver= new AndroidDriver(new URL("htpp://127.0.0.1:4723/wd/hub"),cap);
 	
--- src/Automatisation_bookapp/basic.java
+++ src/Automatisation_bookapp/basic.java
@@ -1,7 +1,22 @@
 package Automatisation_bookapp;
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
 
 @Test
-	public void test() throws MalformedURLException {
+public class basic {
+	public static void basicx() throws MalformedURLException  {
+
+	
 		       File appDir = new File("src");
 		   
 		    File app = new File(appDir, "bookMyShow-ucb.apk");
@@ -17,11 +32,11 @@ package Automatisation_bookapp;
 		driver.findElementById("com.bt.bms:id/btnSignUp").click();
 	int s=driver.findElements(By.className("android.widget.EditText")).size();
 		System.out.println(s);
-		List<WebElement>a=driver.findElements(By.className("android.widget.EditText"));
+		List<WebElement> a=driver.findElements(By.className("android.widget.EditText"));
 		a.get(0).sendKeys("Appium");
 		a.get(1).sendKeys("Mobile");
 		a.get(2).sendKeys("appium@training");
 		a.get(3).sendKeys("password");
 		driver.findElementById("com.bt.bms:id/action_icon").click();
 }
-
+}
